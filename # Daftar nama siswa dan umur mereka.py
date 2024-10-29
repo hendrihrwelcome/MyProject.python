f04b7ecdd9599319355d29f37f850bc262873b66
Hendri Rahman <hendrirahman040517@gmail.com>
2024-10-30 02:26:20 +0700

    Add files via upload

---
+++ # Daftar nama siswa dan umur mereka.py
@@ -0,0 +1,14 @@
+# Daftar nama siswa dan umur mereka
+students = ["Faiz", "Irman", "Aditya", "Rizki", "Taufik", "Aprizal","Raihan","Hendri"]
+ages = [20, 23, 22, 21, 24, 25,19,34]
+
+# Menampilkan nama dan umur siswa pertama
+print(f"Nama {students[7]} umurnya adalah {ages[7]}")
+
+# Menambahkan data baru
+students.append("Brian")
+ages.append(20)
+
+# Menampilkan daftar siswa dan umur setelah penambahan
+print(students)
+print(ages)
